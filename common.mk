8d80d8ddbfe17c712d5c976c7f60e81d0fdc220a
Igor Risteski <pirej30@yahoo.com>
2016-03-29 11:44:20 +0200

    try with xhdpi

--- common.mk
+++ common.mk
@@ -8,7 +8,7 @@ $(call inherit-product-if-exists, $(QCPATH)/common/config/device-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 PRODUCT_BRAND := qcom
-PRODUCT_AAPT_CONFIG += hdpi mdpi
+PRODUCT_AAPT_CONFIG += xhdpi
 
 ifndef PRODUCT_MANUFACTURER
 PRODUCT_MANUFACTURER := QUALCOMM
