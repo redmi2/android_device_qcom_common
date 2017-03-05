d1c42e5c478771915ab145d32b483e3e018fb327
Igor Risteski <pirej30@yahoo.com>
2017-03-05 01:30:42 +0100

    include vendor in the 64 bit build too

--- common64.mk
+++ common64.mk
@@ -3,6 +3,7 @@ $(call inherit-product, device/qcom/common/base.mk)
 # For PRODUCT_COPY_FILES, the first instance takes precedence.
 # Since we want use QC specific files, we should inherit
 # device-vendor.mk first to make sure QC specific files gets installed.
+$(call inherit-product-if-exists, vendor/xiaomi/wt88047/wt88047-vendor.mk)
 $(call inherit-product-if-exists, $(QCPATH)/common/config/device-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
